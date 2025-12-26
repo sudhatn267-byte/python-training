88fb6b8c649623bebb1e5c4ed4d8531fe41a0637
sudhatn267-byte <sudhatn267@gmail.com>
2025-12-26 14:12:50 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,12 @@
+def check(x):
+    l=0
+    r=len(x)-1
+    while l<r:
+        return "not a palindrome"
+    else:
+        l+=1
+        r=-1
+        return "palindrome"
+        x="madamk"
+        print(check(x))
+            
